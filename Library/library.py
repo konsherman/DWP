536bcde67008d96c81ae3b133493a2befabd25a7
Konner <konsherman@fullsail.edu>
2014-11-06 17:25:27 -0500

    library
    completed

--- Library/library.py
+++ Library/library.py
@@ -1,15 +1,31 @@
-#Konner Sherman
+# Konner Sherman
 #DWP
 #Nov 4, 2014
 
 
 class Library(object):
     def __init__(self):
-        self.age = ''
-        self.weight = 0
+        pass
+
+    def calc_info(self,w1,w2,w3,w4):
+        answer = w1 + w2 + w3 + w4 / 4
+        return answer
 
 
 
 class Printer(object):
     def __init__(self):
         pass
+
+    def printstuff(self, a, b):
+        print a + " " + str(b)
+
+
+
+
+
+
+
+
+
+
--- Library/main.py
+++ Library/main.py
@@ -3,18 +3,33 @@
 #Nov 4, 2014
 
 from library import Library
+from library import Printer
+
 
 
 class MainHandler(object):
     def __init__(self):
-        stuff = Library()
-        stuff.age = raw_input("age")
-        print stuff.age
+
+        calculations = Library()
+        name1= raw_input("Name")
+        name2= raw_input("Name")
+        name3= raw_input("Name")
+        name4= raw_input("Name")
+
+        weight1 = int(raw_input("Enter Weight"))
+        weight2 = int(raw_input("Enter Weight"))
+        weight3 = int(raw_input("Enter Weight"))
+        weight4 = int(raw_input("Enter Weight"))
+        thePrinter = Printer()
+        thePrinter.printstuff("The Average Weight of "+ name1 +" "+name2 + " " +name3 + " " +"and"+ name4 + " "+"Is", calculations.calc_info(weight1,weight2,weight3,weight4))
+
+
+
+        
 
 
 
 
 
-#NO MORE GLOBAL VAR'S
-#GLOBAL VARS ARE THE DEVIL
+
 web_app = MainHandler()
