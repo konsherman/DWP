dcc61fa320c27356518c5ea18ba7fee723401867
Konner <konsherman@fullsail.edu>
2014-11-11 17:34:26 -0500

    commenting
    comments

--- Kon_Sherman_Library/library.py
+++ Kon_Sherman_Library/library.py
@@ -8,17 +8,18 @@ class Library(object):
         pass
 
     def calc_info(self,w1,w2,w3,w4):
+        #multiplying the weights and deviding them by 4
         answer = w1 + w2 + w3 + w4 / 4
-        return answer
+        return answer #returning the answers
 
 
 
 class Printer(object):
     def __init__(self):
-        pass
+        pass #passs this function
 
     def printstuff(self, a, b):
-        print a + " " + str(b)
+        print a + " " + str(b) #function for printing out all the information
 
 
 
--- Kon_Sherman_Library/main.py
+++ Kon_Sherman_Library/main.py
@@ -2,7 +2,7 @@
 #DWP
 #Nov 4, 2014
 
-from library import Library
+from library import Library  #importing the libary and printer
 from library import Printer
 
 
@@ -11,7 +11,7 @@ class MainHandler(object):
     def __init__(self):
 
         calculations = Library()
-        name1= raw_input("Name")
+        name1= raw_input("Name")#input for the 4 names
         name2= raw_input("Name")
         name3= raw_input("Name")
         name4= raw_input("Name")
@@ -19,9 +19,9 @@ class MainHandler(object):
         weight1 = int(raw_input("Enter Weight"))
         weight2 = int(raw_input("Enter Weight"))
         weight3 = int(raw_input("Enter Weight"))
-        weight4 = int(raw_input("Enter Weight"))
+        weight4 = int(raw_input("Enter Weight"))#inputs for the 4 weights
         thePrinter = Printer()
-        thePrinter.printstuff("The Average Weight of "+ name1 +" "+name2 + " " +name3 + " " +"and"+ name4 + " "+"Is", calculations.calc_info(weight1,weight2,weight3,weight4))
+        thePrinter.printstuff("The Average Weight of "+ name1 +" "+name2 + " " +name3 + " " +"and"+ name4 + " "+"Is", calculations.calc_info(weight1,weight2,weight3,weight4))#concatination and printing out the information
 
 
 
