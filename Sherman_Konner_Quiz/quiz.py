321dedb41a1f3fdbffc3627f662a5cdc0f6d800d
Konner <konsherman@fullsail.edu>
2014-11-15 22:22:39 -0500

    update on the quiz
    update on the quiz

--- Sherman_Konner_Quiz/quiz.py
+++ Sherman_Konner_Quiz/quiz.py
@@ -9,18 +9,25 @@ class Student(object):
         #-------------
         #---PROTECTED----
 
-        self._age = ""
+        self._age = 0
 
         #^^^PROTECTED^^
         #--------------
         #----PRIVATE----
 
-        self.__height = ""
+        self.__height = 0
 
     def full_student(self):
         #gets the student and adds it into one big student
-        all = self.hair + self.gender + self._age + self.__height
+        all = self.hair + self.gender + str(self._age) + str(self.__height)
         return all
+
+    def math(self):
+        return self._age + int(self.__height)  #adding the self and the age and reutnring it
+
+    def print_out(self):
+        print "Hello world" #printing out hellow world
+
 
 
 class Custodian(object):
@@ -32,8 +39,16 @@ class Custodian(object):
 
     def pay_check(self, hours, pay_rate):
         #this method calulates how much money the person is going to make
-        money = self._hours * 3.00
+        money = hours * pay_rate
         return money
+
+    def run_it(self):
+        print self.pay_check(10, 3.00)
+
+    def test(self):
+        print "Hello World"
+
+
 
 
 
@@ -42,8 +57,14 @@ class Zombie(object):
         self.blood_type = ''
         self.zombie_type = ""
         self.__skin_rot = 0
-        self.__teeth = ""
+        self.__teeth = 0
 
     def print_zombie(self):
         print self.blood_type + self.zombie_type + "The zombie you are fithing has a skin rotting level of" + str(self.__skin_rot) + self.__teeth
+
+    def mic_check(self):
+        print "Mic Check"
+
+    def print_out(self):
+        print self.__teeth + self.__skin_rot
 
