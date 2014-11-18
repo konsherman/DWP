46684d9dd76e6c994d4a9144c3e67cb193b6e9d2
Konner <konsherman@fullsail.edu>
2014-11-17 21:30:27 -0500

    forgot some comments
    Final comments

--- getset/main.py
+++ getset/main.py
@@ -7,13 +7,14 @@ from count import Count
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        c = Count()
+        c = Count() #bring in the count class
 
-        if self.request.GET:
+        if self.request.GET: #when GET print the self.content and evyerhitng
             c.count = int(self.request.GET['n'])
 
 
         self.response.write(c.print_out())
+        #else just print out what its got
 
 
 app = webapp2.WSGIApplication([
